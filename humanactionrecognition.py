9fb876d367aa9b440a324f87dc95d29af51426d2
moon_well <moon_well@live.cn>
2019-03-20 18:11:59 +0800

    batch normlization是有毒，我感觉这个玩意不能使用。。。会出问题，

--- humanactionrecognition.py
+++ humanactionrecognition.py
@@ -12,7 +12,7 @@ import scipy.io as sio
 
 #train setting
 n_epochs = 10
-n_episodes = 400
+n_episodes = 2000
 n_classes=27
 
 n_sample_per_class=32
@@ -58,21 +58,21 @@ def encoder(x, h_dim, z_dim, reuse=False):
         #---------#
         block_1_in=x
         block_1_out = tf.layers.conv2d(block_1_in, h_dim, kernel_size=[2, 3], dilation_rate=[2, 2],padding='SAME')  # 64 filters, each filter will generate a feature map.
-        block_1_out = tf.contrib.layers.batch_norm(block_1_out, updates_collections=None, decay=0.99, scale=True, center=True)
+        #block_1_out = tf.contrib.layers.batch_norm(block_1_out, updates_collections=None, decay=0.99, scale=True, center=True)
         block_1_out = tf.nn.relu(block_1_out)
         #---------#
 
         #---------#
         block_2_in = tf.concat([block_1_out, block_1_in], axis=3)
         block_2_out = tf.layers.conv2d(block_2_in, h_dim, kernel_size=[2, 3], dilation_rate=[2, 2],padding='SAME')
-        block_2_out = tf.contrib.layers.batch_norm(block_2_out, updates_collections=None, decay=0.99, scale=True,center=True)
+        #block_2_out = tf.contrib.layers.batch_norm(block_2_out, updates_collections=None, decay=0.99, scale=True,center=True)
         block_2_out = tf.nn.relu(block_2_out)
         #---------#
 
         #---------#
         block_3_in = tf.concat([block_2_out, block_1_out,block_1_in], axis=3)
         block_3_out = tf.layers.conv2d(block_3_in, h_dim, kernel_size=[2, 3], dilation_rate=[2, 2],padding='SAME')
-        block_3_out = tf.contrib.layers.batch_norm(block_3_out, updates_collections=None, decay=0.99, scale=True,center=True)
+        #block_3_out = tf.contrib.layers.batch_norm(block_3_out, updates_collections=None, decay=0.99, scale=True,center=True)
         block_3_out = tf.nn.relu(block_3_out)
         #---------#
 
