6f43d5a164b36555bc761b1e509302598a1141cb
moon_well <moon_well@live.cn>
2019-04-04 19:48:33 +0800

    优化代码结构

---
+++ humanactionrecognition_unseen_class_Tr5_In5.py
@@ -0,0 +1,13 @@
+# -*- encoding=utf-8 -*-
+# matplotlib inline
+from __future__ import print_function
+import UNSeen
+
+
+'''
+training:利用类编号为1-10的样本,使用5个support样本，利用5个query,对模型进行训练，
+inference:利用类编号为11-27的样本,使用5个support样本，利用27个query,对模型进行评估，
+'''
+
+UNSeen.n_test_way = 5
+UNSeen.train_test()
