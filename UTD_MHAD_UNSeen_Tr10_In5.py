2353045ade8eca7c1321e5f11db3eb560dbb1666
moon_well <moon_well@live.cn>
2019-04-06 16:33:55 +0800

    add coder

--- UTD_MHAD_UNSeen_Tr10_In5.py
+++ UTD_MHAD_UNSeen_Tr10_In5.py
@@ -9,6 +9,6 @@ training:利用类编号为1-10的样本,使用5个support样本，利用5个que
 inference:利用类编号为11-27的样本,使用5个support样本，利用27个query,对模型进行评估，
 '''
 UTD_MHAD_UNSeen.n_episodes=2000
-UTD_MHAD_UNSeen.n_test_way = 10
-UTD_MHAD_UNSeen.n_way=5
+UTD_MHAD_UNSeen.n_test_way = 5
+UTD_MHAD_UNSeen.n_way=10
 UTD_MHAD_UNSeen.train_test()
