abd69569e8e511774fdb5e964bf436a8f736a946
moon_well <moon_well@live.cn>
2019-04-08 15:42:36 +0800

    fix bug

--- UTD_MHAD_4_Support.py
+++ UTD_MHAD_4_Support.py
@@ -1,3 +1,5 @@
 import UTD_MHAD
+import os
+UTD_MHAD.ckpt_path='./ckpt/%s'%os.path.basename(__file__)
 UTD_MHAD.n_episodes=400
 UTD_MHAD.train_test()
--- UTD_MHAD_UNSeen_Tr10_In5.py
+++ UTD_MHAD_UNSeen_Tr10_In5.py
@@ -8,6 +8,8 @@ import UTD_MHAD_UNSeen
 training:利用类编号为1-10的样本,使用5个support样本，利用5个query,对模型进行训练，
 inference:利用类编号为11-27的样本,使用5个support样本，利用27个query,对模型进行评估，
 '''
+import os
+UTD_MHAD_UNSeen.ckpt_path='./ckpt/%s'%os.path.basename(__file__)
 UTD_MHAD_UNSeen.n_episodes=150
 UTD_MHAD_UNSeen.n_test_way = 5
 UTD_MHAD_UNSeen.n_way=10
