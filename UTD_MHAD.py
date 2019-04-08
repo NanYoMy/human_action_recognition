d7268a37b6d31457a9aea39cc806fffd7a02d9dd
moon_well <moon_well@live.cn>
2019-04-08 15:30:00 +0800

    ckpt的设置

--- UTD_MHAD.py
+++ UTD_MHAD.py
@@ -30,6 +30,7 @@ n_test_query = n_sample_per_class - n_support - n_query#n_test_shot+n_test_query
 im_height,im_width,  channels = 20, 60, 3
 h_dim = 8
 z_dim = 64
+ckpt_path='./ckpt/untitled'
 def euclidean_distance(query=None, prototype=None): # a是query b是protypical
     # a.shape = Class_Number*Query x D
     # b.shape = Class_Number x D
@@ -191,15 +192,15 @@ def train_test():
     print(train_dataset.shape)#(10, 32, 60, 40, 3)
     print(test_dataset.shape)#(10, 32, 60, 40, 3)
 
-    x = tf.placeholder(tf.float32, [None, None, im_height, im_width, channels])
-    q = tf.placeholder(tf.float32, [None, None, im_height, im_width, channels])
+    x = tf.placeholder(tf.float32, [None, None, im_height, im_width, channels],name='x')
+    q = tf.placeholder(tf.float32, [None, None, im_height, im_width, channels],name='q')
     x_shape = tf.shape(x)
     q_shape = tf.shape(q)
     #训练的时候具有support sample的参数
     num_classes, num_support = x_shape[0], x_shape[1]# num_class num_support_sample
     num_queries = q_shape[1]#num_query_sample
     #y为label数据由外部导入
-    y = tf.placeholder(tf.int64, [None, None])
+    y = tf.placeholder(tf.int64, [None, None],name='y')
     y_one_hot = tf.one_hot(y, depth=num_classes)# dimesion of each one_hot vector
     #emb_x是样本通过encoder之后的结果
     emb_x = encoder(tf.reshape(x, [num_classes * num_support, im_height, im_width, channels]), h_dim, z_dim,reuse=False)
@@ -211,11 +212,13 @@ def train_test():
     dists = euclidean_distance(emb_q, emb_x)
 
     log_p_y = tf.reshape(tf.nn.log_softmax(-dists), [num_classes, num_queries, -1])#-1表示自动计算剩余维度，paper中公式2 log_softmax 默认 axis=-1
-    ce_loss = -tf.reduce_mean(tf.reshape(tf.reduce_sum(tf.multiply(y_one_hot, log_p_y), axis=-1), [-1]))#reshpae(a,[-1])会展开所有维度, ce_loss=cross entropy
-    acc = tf.reduce_mean(tf.to_float(tf.equal(tf.argmax(log_p_y, axis=-1), y)))
+    ce_loss = -tf.reduce_mean(tf.reshape(tf.reduce_sum(tf.multiply(y_one_hot, log_p_y), axis=-1), [-1]),name='loss')#reshpae(a,[-1])会展开所有维度, ce_loss=cross entropy
+    acc = tf.reduce_mean(tf.to_float(tf.equal(tf.argmax(log_p_y, axis=-1), y)),name='acc')
 
-
+    tf.add_to_collection('acc', acc)
+    tf.add_to_collection('loss', ce_loss)
     train_op = tf.train.AdamOptimizer().minimize(ce_loss)
+    saver = tf.train.Saver()
     sess = tf.InteractiveSession()
     init_op = tf.global_variables_initializer()
     sess.run(init_op)
@@ -240,8 +243,7 @@ def train_test():
         #if (epi + 1) %50 == 0:
         print('[ episode {}/{}] => loss: {:.5f}, acc: {:.5f} '.format(epi + 1,n_episodes,ls,ac))
 
-
-
+    saver.save(sess, ckpt_path)
     print('Testing normal classes...')
     avg_acc = 0.
     avg_ls=0.
@@ -273,3 +275,41 @@ def train_test():
     #， 测试的修改：分27类 现在每次测试都是从32个样本里面随机抽5个当作支持向量，检验剩余27样本的数据，
     图片的生成：现在每次都是从sequnce中生成一张图片，能否生成多张图片？
     '''
+def load_test():
+
+    data_addr = sorted(glob.glob('.\\data\\Skeleton\\data\\*.mat'))  # all data
+    test_dataset, train_dataset = prepar_data(data_addr, n_classes)
+    print(test_dataset.shape)
+    sess = tf.Session()
+    saver = tf.train.import_meta_graph('%s.meta'%ckpt_path)
+    saver.restore(sess,ckpt_path)
+    graph = tf.get_default_graph()
+    x=graph.get_operation_by_name('x').outputs[0]
+    y=graph.get_operation_by_name('y').outputs[0]
+    q =graph.get_operation_by_name('q').outputs[0]
+    ce_loss=tf.get_collection('loss')[0]
+    acc=tf.get_collection('acc')[0]
+    avg_acc = 0.
+    avg_ls=0.
+    for epi in range(n_test_episodes):
+        epi_classes = np.random.permutation(n_classes)[:n_test_way]
+        support = np.zeros([n_test_way, n_test_support, im_height, im_width, channels], dtype=np.float32)
+        query = np.zeros([n_test_way, n_test_query, im_height, im_width,channels], dtype=np.float32)
+        for i, epi_cls in enumerate(epi_classes):
+
+            selected_support = np.random.permutation(n_query+n_support)[:n_test_support]#从训练集合取support样本
+            selected_query = np.random.permutation(n_test_query)#22个样本
+            support[i] = train_dataset[epi_cls, selected_support]#从训练集合取support样本
+            query[i] = test_dataset[epi_cls, selected_query]
+        # support = np.expand_dims(support, axis=-1)
+        # query = np.expand_dims(query, axis=-1)
+        labels = np.tile(np.arange(n_test_way)[:, np.newaxis], (1, n_test_query)).astype(np.uint8)
+        ls, ac = sess.run([ce_loss, acc], feed_dict={x: support, q: query, y:labels})
+
+        avg_acc += ac
+        avg_ls+=ls
+        if (epi+1) % 50 == 0:
+            print('[test episode {}/{}] => loss: {:.5f}, acc: {:.5f} '.format(epi+1, n_test_episodes, ls, ac))
+    avg_acc /= n_test_episodes
+    avg_ls/=n_test_episodes
+    print('Average Test Accuracy: {:.5f} Average loss : {:.5f}'.format(avg_acc,avg_ls))
