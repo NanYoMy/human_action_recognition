0f77bd2c9f6de1a589b7c625ded897e933abb019
moon_well <moon_well@live.cn>
2019-04-09 18:35:58 +0800

    add kard

--- KARD_UNSEEN_Tr10_In8_5support.py
+++ KARD_UNSEEN_Tr10_In8_5support.py
@@ -9,4 +9,4 @@ KARD_UNSeen.n_test_classes=8
 KARD_UNSeen.n_test_way=8
 KARD_UNSeen.AS_Train=np.array([0,1,2,3,4,5,6,7,8,9])
 KARD_UNSeen.AS_Test=np.array([10,11,12,13,14,15,16,17])
-KARD_UNSeen.train_test()
+KARD_UNSeen.load_test()
