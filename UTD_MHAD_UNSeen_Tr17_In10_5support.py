9d2bf8915444ff7b058ff2da724b820e4f316150
moon_well <moon_well@live.cn>
2019-04-16 14:11:52 +0800

    fix bug

--- UTD_MHAD_UNSeen_Tr17_In10_5support.py
+++ UTD_MHAD_UNSeen_Tr17_In10_5support.py
@@ -16,7 +16,7 @@ UTD_MHAD_UNSeen.n_test_classes=10
 UTD_MHAD_UNSeen.n_way=17
 UTD_MHAD_UNSeen.n_test_way=10
 #UTD_MHAD_UNSeen.n_test_support=1
-UTD_MHAD_UNSeen.n_test_support=5
+UTD_MHAD_UNSeen.n_test_support=1
 UTD_MHAD_UNSeen.n_test_query=UTD_MHAD_UNSeen.n_sample_per_class-UTD_MHAD_UNSeen.n_test_support
 UTD_MHAD_UNSeen.n_test_episodes=1000
 
