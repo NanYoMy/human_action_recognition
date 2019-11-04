b052829640e2292eac1317e64eceea8b55da0613
nanyomy <moon_well@live.cn>
2019-11-04 11:18:49 +0800

    UTD_MHAD的实验1完成

--- UTD_MHAD_27way_1shot.py
+++ UTD_MHAD_27way_1shot.py
@@ -1,12 +1,12 @@
 import UTD_MHAD
 import os
 UTD_MHAD.ckpt_path='./ckpt/%s'%os.path.basename(__file__)
-UTD_MHAD.n_episodes=1000
-UTD_MHAD.n_way = 20
-UTD_MHAD.n_support=5
-UTD_MHAD.n_query=5
-UTD_MHAD.n_test_way=20
-UTD_MHAD.n_test_support=5
+UTD_MHAD.n_episodes=1500
+UTD_MHAD.n_way = 27
+UTD_MHAD.n_support=1
+UTD_MHAD.n_query=1
+UTD_MHAD.n_test_way=27
+UTD_MHAD.n_test_support=1
 UTD_MHAD.n_test_query=int(UTD_MHAD.n_sample_per_class/2)-UTD_MHAD.n_test_support
 
 
