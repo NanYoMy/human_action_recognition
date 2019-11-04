f213b8da11888db65f384662e9e74c66dfc0f2ed
nanyomy <moon_well@live.cn>
2019-11-04 15:55:51 +0800

    ablation study

---
+++ UTD_MHAD_ori_20way_5shot.py
@@ -0,0 +1,13 @@
+import UTD_MHAD_original as UTD_MHAD
+import os
+UTD_MHAD.ckpt_path='./ckpt/%s'%os.path.basename(__file__)
+UTD_MHAD.n_episodes=1000
+UTD_MHAD.n_way = 20
+UTD_MHAD.n_support=5
+UTD_MHAD.n_query=5
+UTD_MHAD.n_test_way=20
+UTD_MHAD.n_test_support=5
+UTD_MHAD.n_test_query=int(UTD_MHAD.n_sample_per_class/2)-UTD_MHAD.n_test_support
+
+
+UTD_MHAD.train_test()
